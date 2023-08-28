b7608a1537e0435c90e38d3d89033b263caa0c0f
JeffTheK <jeff144@protonmail.com>
2023-08-28 14:40:33 +0300

    .

--- flumix/stdlib/special.py
+++ flumix/stdlib/special.py
@@ -4,6 +4,10 @@ from .. import interpreter
 def env_contains(args, env):
     return env.find(args[0]) != None
 
+def global_env(args, env):
+    return env.outer
+
 STDLIB_SPECIAL = {
-    "special/env-contains": PythonFunction("special/env-contains", env_contains, False),
+    "special/global-env": PythonFunction("special/global-env", global_env, False),
+    "special/env-contains": PythonFunction("special/env-contains", env_contains, False)
 }
