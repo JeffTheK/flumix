5b132d2f78af93ed87a4d18532fb105022105b47
JeffTheK <jeff144@protonmail.com>
2023-08-30 08:01:39 +0300

    .

--- flumix/stdlib/list.py
+++ flumix/stdlib/list.py
@@ -9,6 +9,12 @@ class List(list):
 def _list_new(args, env):
     return List(args)
 
+def _get_at(args, env):
+    list = args[0]
+    index = args[1]
+    return list[index]
+
 STDLIB_LIST = {
     "list/new": PythonFunction("list/new", _list_new, True, [], any_number_of_args=True),
+    "list/get-at": PythonFunction("list/get-at", _get_at, True, ["list", "index"])
 }
--- tests/test_list.py
+++ tests/test_list.py
@@ -1,6 +1,6 @@
 from flumix.interpreter import exec_string
 
-def test_function_no_args(capsys):
+def test_new(capsys):
     code = """(do 
     (print (list/new 1 2 3 4 5))
 )
@@ -8,3 +8,15 @@ def test_function_no_args(capsys):
     exec_string(code)
     captured = capsys.readouterr()
     assert(captured.out == "[1, 2, 3, 4, 5]\n")
+
+def test_get_at(capsys):
+    code = """(do 
+    (var l (list/new 1 2 3 4 5))
+    (print (list/get-at l 0))
+    (print (list/get-at l 1))
+    (print (list/get-at l -1))
+)
+"""
+    exec_string(code)
+    captured = capsys.readouterr()
+    assert(captured.out == "1\n2\n5\n")
